5df47bbb1c3f602f495dcfa381483e72582a9fdf
zafar2199 <75061841+zafar2199@users.noreply.github.com>
2021-01-13 13:32:34 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+// const text = document.getElementById("text");
+// const prog = "<p> Hamburg :) ......................</p>";
+// let idx = 1;
+
+// setInterval(writeText, 100);
+
+// function writeText() {
+//   text.innerText = prog.slice(0,idx);
+
+//   idx++;
+
+//   if(idx>prog.length){
+//     idx = 1;
+//   }
+// }
